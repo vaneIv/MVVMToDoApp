94b191442be599b021936a2d7697a29e66c96910
ivVane <iv.vane@gmail.com>
2022-10-30 20:16:22 +0100

    Combining multiple flows(three in our case), created and implemented sorting functions.

--- app/src/main/java/com/example/mvvmtodoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/example/mvvmtodoapp/ui/tasks/TasksViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.asLiveData
 import com.example.mvvmtodoapp.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
@@ -15,10 +16,21 @@ class TasksViewModel @Inject constructor(
 
     val searchQuery = MutableStateFlow("")
 
-    private val taskFlow = searchQuery.flatMapLatest {
-        taskDao.getTask(it)
+    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+    val hideCompleted = MutableStateFlow(false)
+
+    private val taskFlow = combine(
+        searchQuery,
+        sortOrder,
+        hideCompleted
+    ) { query, sortOrder, hideCompleted ->
+        Triple(query, sortOrder, hideCompleted)
+    }.flatMapLatest { (query, sortOrder, hideCompleted) ->
+        taskDao.getTasks(query, sortOrder, hideCompleted)
     }
 
     val tasks = taskFlow.asLiveData()
 
 }
+
+enum class SortOrder { BY_NAME, BY_DATE }
