387820811e4fcdec1756171deba1fef8d2446014
ivVane <iv.vane@gmail.com>
2023-02-04 14:26:21 +0100

    Created and configured functionality to delete all completed tasks and Dialog for the same functionality.

--- app/src/main/java/com/example/mvvmtodoapp/data/TaskDao.kt
+++ app/src/main/java/com/example/mvvmtodoapp/data/TaskDao.kt
@@ -6,8 +6,8 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface TaskDao {
 
-    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean) : Flow<List<Task>> =
-        when(sortOrder) {
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+        when (sortOrder) {
             SortOrder.BY_DATE -> getTasksSortedByDateCreated(query, hideCompleted)
             SortOrder.BY_NAME -> getTasksSortedByName(query, hideCompleted)
         }
@@ -26,4 +26,7 @@ interface TaskDao {
 
     @Delete
     suspend fun delete(task: Task)
+
+    @Query("DELETE FROM task_table WHERE completed = 1")
+    suspend fun deleteCompletedTasks()
 }
